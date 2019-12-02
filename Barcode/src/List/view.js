9c2b2ff0fc2ac83066f6b76e9a0207327a9ab6ed
nikfaiz97 <nfaizfauzi@gmail.com>
2019-12-02 08:09:18 +0600

    Styling view

--- Barcode/src/List/view.js
+++ Barcode/src/List/view.js
@@ -3,12 +3,13 @@ import {
     View,
     Text,
     StyleSheet,
+    TouchableOpacity,
+    StatusBar
 } from "react-native";
-
 import SplashScreen from 'react-native-splash-screen';
-
 import {app} from '../fbconfig';
-
+import { Colors } from "react-native-paper";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default class view extends Component {
     constructor(props){
@@ -35,16 +36,30 @@ export default class view extends Component {
             // // let data = snapshot.val()
             // console.log(`Data: ${snapshot.val().key.Link}`)
             console.log((this.state.content))
-            
         });
     }
+    
     render() {
         return(
+            
             <View>
+              <StatusBar backgroundColor="#239b56" barStyle="light-content"/>
+              <ScrollView>
+                <View style={styles.title}>
+                  <Text style={{color:'white', fontWeight: 'bold'}}>Barcode List</Text>
+                </View>
                 {this.state.content.map((y)=>{
-                return(<Text>{y.Link}</Text>);
+                return(
+                <TouchableOpacity>
+                    <View style={styles.listItem}>
+                      <Text style={{color:'black'}}>{y.Link}</Text>
+                    </View>
+                </TouchableOpacity>
+                  );
                 })}
-            </View>
+              </ScrollView>
+           </View>
+           
         )
     }
 }
@@ -55,5 +70,18 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       backgroundColor: '#ebebeb'
+    },
+    title: {
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#239b56',
+    },
+    listItem: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#239b56',
+        // backgroundColor: '#2ecc71',
+        // marginTop: 10,
+        // marginHorizontal: 50
     }
   });
